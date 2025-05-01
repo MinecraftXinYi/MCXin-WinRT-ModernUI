3288c29d93001863b97d36a76ac63be46e081735
MinecraftXinYi <3077501050@qq.com>
2025-05-01 11:14:16 +0800

    ...

---
+++ MCXin.WinRT.ModernUI.Core.Common/Direct/WRTUITextInputNative.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MinecraftXinYi.Windows.ModernUI.Core.Direct;
+
+using MetaData;
+
+public static partial class WRTUITextInputNative
+{
+    [DllImport(WRTUINativeLibraryName.WUCTI, EntryPoint = WRTUINativeLibraryEntryPoint.DefaultEntryPoint)]
+    public static extern int CreateTextInputProducer(nint consumer, out nint pProducer);
+}
